4771f210bcde9b7b349ad24a51e8ea52caaced64
Gabriel <gapdmm@gmail.com>
2024-10-16 19:02:30 -0300

    feat: implementado API ViaCep no formulário

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,28 @@
-const toggleContainer = document.querySelector('.toggle-container') as HTMLElement;
-const toggleButton = document.getElementById('buttonToggle') as HTMLElement;
-const lampada = document.getElementById('lampada') as HTMLImageElement;
-const limiteLampada = document.getElementById('limiteLampada') as HTMLSpanElement;
-const body = document.querySelector<HTMLBodyElement>('body');
-let isOn: boolean = false;
-const limiteClicks: number = Math.floor(Math.random() * 8);
-let contador: number = 0;
-let limiteLampadaTexto: number = limiteClicks;
+const cep = document.getElementById('cep') as HTMLInputElement;
+const logradouro = document.getElementById('logradouro') as HTMLInputElement;
+const cidade = document.getElementById('cidade') as HTMLInputElement;
+const estado = document.getElementById('estado') as HTMLInputElement;
+const labelCep = document.getElementById('labelCep') as HTMLLabelElement;
 
-limiteLampada.textContent = `A lampada pode ser ativada ${limiteLampadaTexto} vezes`;
+cep.addEventListener('blur', () => {
+    fetch(`https://viacep.com.br/ws/${cep.value}/json/`)
+    .then(resposta => resposta.json())
+    .then(endereco => {
+        logradouro.value = endereco.logradouro === undefined ? '' : endereco.logradouro;
+        cidade.value = endereco.localidade === undefined ? '' : endereco.localidade;
+        estado.value = endereco.uf === undefined ? '' : endereco.uf
 
-toggleContainer.addEventListener('click', () => {
-  isOn = !isOn;
-  
-  if (isOn && body) {
-      toggleButton.style.left = '33px';
-      toggleContainer.style.backgroundColor = '#2196F3';
-      contador++; 
-      limiteLampadaTexto--;
-      limiteLampada.textContent = `A lampada pode ser ativada ${limiteLampadaTexto} vezes`;
-      body.style.backgroundColor = '#fffaca';
+        if(endereco.logradouro === undefined || endereco.localidade === undefined || endereco.uf === undefined) {
+            labelCep.innerText = 'CEP Inválido';
+            labelCep.style.color = 'red';
+       
+        }else{
+            labelCep.innerText = 'CEP';
+            labelCep.style.color = '#4e515a';
+        }
+    })
+    .catch(erro => console.log(erro))
+})
 
-      if(limiteLampadaTexto < 0) {
-        limiteLampada.textContent = `A lampada está quebrada!`;
-      }
-    } else {
-        toggleButton.style.left = '5px';
-        toggleContainer.style.backgroundColor = '#CCC';
-        body!.style.backgroundColor = '#1a1a1a'; 
-    }
-    
-    if(lampada && (contador > limiteClicks) && body) {
-        lampada.src = './assets/img/lampada-quebrada.png';
-        body.style.backgroundColor = '#1a1a1a';
-    } else if(lampada && isOn) {
-        lampada.src = './assets/img/lampada-ligada.png';
-    }else {
-        lampada.src = './assets/img/lampada-desligada.png';
-    }    
-});
+
 
